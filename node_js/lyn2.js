e5eaa863ff28e031d50d23b394d458081682131a
DY Kim <dykim@DYui-MacBook-Pro.local>
2016-02-05 17:37:03 +0900

    Remove files which is outside of each folder

--- node_js/lyn2.js
+++
@@ -1,8 +0,0 @@
-args = process.argv;
-
-var sum = 0;
-for (var i=2; i<args.length; i++) {
-  sum += +args[i];
-}
-
-console.log(sum);
--- node_js/lyn3.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-var lines = fs.readFileSync(process.argv[2])
-                .toString()
-                .split('\n');
-
-console.log(lines.length-1);
